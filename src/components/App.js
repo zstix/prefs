9957913274677959bdabd668ef16b5473f32113c
Zack Stickles <zstickles@gmail.com>
2020-06-06 11:16:27 -0700

    wip: working on showing options

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,26 @@
 import React from "react";
+import Instructions from "./Instructions";
 
-const App = () => (
-  <div className="wrapper">
-    <div className="instructions">
-      <h1>Personal Preference!</h1>
-      <h2>Rules:</h2>
+import activities from "../data/activities";
+import potpourri from "../data/potpourri";
+import foodAndDrink from "../data/food-and-drink";
+import people from "../data/people";
 
-      <h3>1. Choose a Person</h3>
-      <p>Choose a person (take turns!) who everyone will guess for.</p>
+const App = () => {
+  const categories = [
+    { name: "Activities", options: activities },
+    { name: "Potpourri", options: potpourri },
+    { name: "Food & Drink", options: foodAndDrink },
+    { name: "People", options: people },
+    { name: "One of Each", options: [] },
+  ];
 
-      <h3>2. Choose a Category</h3>
-      <p>
-        The person you are gessing for chooses a category. Click the title for
-        that category to see new items. The person who chooses the category will
-        then secretly pick their preference between the four items in that
-        category, ranking them from <i>most</i> prefered to <i>least</i>{" "}
-        prefered.
-      </p>
-
-      <h3>3. Guessing Round</h3>
-      <p>
-        Each other player will write down their guess. Once all players have
-        thier guesses in, the other players take turns saying their guesses (and
-        their reasons for them). After all other players have gone, the person
-        they are guessing for reveals their preferences.
-      </p>
-
-      <h3>4. Points &amp; Winning</h3>
-      <p>
-        1 point per correct answer, no negative points if you're wrong - pick
-        what number to play up to and declare a winner
-      </p>
+  return (
+    <div className="wrapper">
+      <div className="categories">categories go here</div>
+      <Instructions />
     </div>
-
-    <div className="categories">categories go here</div>
-  </div>
-);
+  );
+};
 
 export default App;
