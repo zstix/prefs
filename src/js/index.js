ab9823fb59eef173a46c2b7bb0ede7b05b3c0bde
Zack Stickles <zstickles@gmail.com>
2020-06-05 18:02:06 -0700

    chore: initial messaging

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <p>Hello, React!</p>;
+const App = () => <>Personal Preference</>;
 
 const $container = document.getElementById("app");
 ReactDOM.render(<App />, $container);
