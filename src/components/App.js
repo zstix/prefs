2388b7c6636d6f60ce232f18c01e69ae267d86d7
Zack Stickles <zstickles@gmail.com>
2020-06-06 11:33:02 -0700

    feat: random items

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import shuffle from "array-shuffle";
 import Instructions from "./Instructions";
 import Categories from "./Categories";
 
@@ -9,11 +10,19 @@ import people from "../data/people";
 
 const App = () => {
   const categories = [
-    { name: "Activities", options: activities.slice(0, 4) },
-    { name: "Potpourri", options: potpourri.slice(0, 4) },
-    { name: "Food & Drink", options: foodAndDrink.slice(0, 4) },
-    { name: "People", options: people.slice(0, 4) },
-    { name: "One of Each", options: [] },
+    { name: "Activities", options: shuffle(activities).slice(0, 4) },
+    { name: "Potpourri", options: shuffle(potpourri).slice(0, 4) },
+    { name: "Food & Drink", options: shuffle(foodAndDrink).slice(0, 4) },
+    { name: "People", options: shuffle(people).slice(0, 4) },
+    {
+      name: "One of Each",
+      options: shuffle([
+        shuffle(activities).slice(-1),
+        shuffle(potpourri).slice(-1),
+        shuffle(foodAndDrink).slice(-1),
+        shuffle(people).slice(-1),
+      ]),
+    },
   ];
 
   return (
